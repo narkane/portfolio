465aef33bb95bad343d058c79e5deae769d000ac
Hans Thummel <enikath@gmail.com>
2019-05-20 11:38:13 -0500

    code reorganization (breaking up Header -became-> Dashboard.js)

--- src/Components/BurgerBox/BurgerBox.js
+++ src/Components/BurgerBox/BurgerBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import NewsCard from "../NewsCard";
 
 class BurgerBox extends Component {
@@ -9,7 +10,44 @@ class BurgerBox extends Component {
       burger: false
     };
     this.toggleBurg = this.toggleBurg.bind(this);
+    this.logout = this.logout.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
   }
+
+  logout = () => {
+    const { updateLoggedIn } = this.props;
+
+    axios.get("http://sdc.thummel.site:3004/logout").then(resp => {
+      console.log(resp);
+      if (resp.status === 200) {
+        console.log("loggin out");
+        //this.setState({ loggedIn: false });
+        updateLoggedIn(false);
+      }
+    });
+    // return logged;
+  };
+
+  deleteUser = (u, p) => {
+    const { updateLoggedIn } = this.props;
+    axios
+      .post("http://sdc.thummel.site:3004/delete", {
+        username: u,
+        password: p
+      })
+      .then(resp => {
+        console.log(resp.status + ": " + JSON.stringify(resp.data));
+        if (resp.status === 200) {
+          console.log("deleting");
+          //this.setState({ loggedIn: false });
+          updateLoggedIn(false);
+        }
+      })
+      .catch(e => {
+        console.log("ERROR: " + e);
+      });
+    // return logged;
+  };
 
   toggleBurg = () => {
     this.setState({ burger: !this.state.burger });
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -15,48 +15,11 @@ class Dashboard extends Component {
     super(props);
     this.state = {};
 
-    this.logout = this.logout.bind(this);
-    this.deleteUser = this.deleteUser.bind(this);
     //this.change_name = this.change_name.bind(this);
     // webMethods = new webMethods();
     // const { updateLoggedIn } = this.props;
     axios.defaults.withCredentials = true;
   }
-
-  logout = () => {
-    const { updateLoggedIn } = this.props;
-
-    axios.get("http://sdc.thummel.site:3004/logout").then(resp => {
-      console.log(resp);
-      if (resp.status === 200) {
-        console.log("loggin out");
-        //this.setState({ loggedIn: false });
-        updateLoggedIn(false);
-      }
-    });
-    // return logged;
-  };
-
-  deleteUser = (u, p) => {
-    const { updateLoggedIn } = this.props;
-    axios
-      .post("http://sdc.thummel.site:3004/delete", {
-        username: u,
-        password: p
-      })
-      .then(resp => {
-        console.log(resp.status + ": " + JSON.stringify(resp.data));
-        if (resp.status === 200) {
-          console.log("deleting");
-          //this.setState({ loggedIn: false });
-          updateLoggedIn(false);
-        }
-      })
-      .catch(e => {
-        console.log("ERROR: " + e);
-      });
-    // return logged;
-  };
 
   //////////////////////////
   // RENDER ////////////////
@@ -66,7 +29,11 @@ class Dashboard extends Component {
     return (
       <div className="Dashboard">
         {/* <div className="title">Software Development</div> */}
-        {this.props.loggedIn ? <BurgerBox /> : <LoginArea />}
+        {this.props.loggedIn ? (
+          <BurgerBox showDP={this.props.showDP} />
+        ) : (
+          <LoginArea />
+        )}
       </div>
     );
   }
